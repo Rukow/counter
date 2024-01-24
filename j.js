7332bf522f32bfbe2780b8875cc478236089e1ad
mateen8325 <156707376+mateen8325@users.noreply.github.com>
2024-01-25 01:42:16 +0500

    Add files via upload

---
+++ j.js
@@ -0,0 +1,32 @@
+
+// conuter program
+
+
+ const decreasebtn = document.getElementById("decreasebtn");
+ const resetbtn = document.getElementById("resetbtn");
+ const increasebtn = document.getElementById("increasebtn");
+ const countLable = document.getElementById("countLable");
+
+
+ let count =0;
+
+
+ increasebtn.onclick = function(){
+ count++;
+ countLable.textContent =count;
+
+
+ }
+ 
+ resetbtn.onclick = function(){
+    count=0;
+    countLable.textContent =count;
+   
+    }
+
+
+    decreasebtn.onclick = function(){
+        count--;
+        countLable.textContent =count;
+       
+        }
